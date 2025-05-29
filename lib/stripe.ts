beb4add014520987a63ab9ec288cf4ff2f142b18
Zihadul Islam <zihadislam2429@gmail.com>
2025-05-30 00:04:27 +0600

    fix: update stripe

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -5,7 +5,8 @@ if (!process.env.STRIPE_SECRET_KEY) {
 }
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: "2025-03-31.basil",
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  apiVersion: "2022-11-15" as any,
 });
 
 export default stripe;
